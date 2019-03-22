53d4671e3fa5af06918709d16bd412fe5b71208f
WEIBEL <74720@GFPS.Local>
2019-03-22 15:07:36 -0600

    3 22 19

--- button/Test/buttoncolor.js
+++ button/Test/buttoncolor.js
@@ -13,10 +13,3 @@ button.onclick = function() {
 	count += count;
 	button.innerHTML = "Click me: " + count;
 };
-
-var h2 = document.getElementById("starter"),
-	count = 1;
-h2.onclick="Myfunction()" {
-	count += 1000;
-	h2.innerHTML = count;
-};
