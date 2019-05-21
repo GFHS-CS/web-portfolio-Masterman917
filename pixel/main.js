1c9af6ec433a0c958ffe17c9bb70169fc589dbac
WEIBEL <74720@GFPS.Local>
2019-05-21 15:04:07 -0600

    5 21 19

---
+++ pixel/main.js
@@ -0,0 +1,19 @@
+/*
+ * Auto-generated content from the Brackets New Project extension.
+ */
+
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
+/*global $, window, document */
+
+// Simple jQuery event handler
+var penColour = 'black';
+
+function setPenColour(pen)
+{
+	penColour = pen;
+}
+
+function setPixelColour(pixel)
+{
+	pixel.style.backgroundColor = penColour;
+}
